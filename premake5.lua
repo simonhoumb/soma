f3a67f7d6e6e1723b821fc2789b0108da8ddf8df
Simon <simon.houmb.2302@gmail.com>
2024-12-17 22:16:25 +0100

    chore: added 'glfw' include to premake file

--- premake5.lua
+++ premake5.lua
@@ -1,103 +1,117 @@
 workspace "Soma"
-    architecture "x64"
+	architecture "x64"
 
-    configurations {
-        "Debug",
-        "Release",
-        "Dist"
-    }
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+IncludeDir = {}
+IncludeDir["GLFW"] = "Soma/vendor/GLFW/include"
+
+include "Soma/vendor/GLFW"
+
 project "Soma"
-    location "Soma"
-    kind "SharedLib"
-    language "C++"
+	location "Soma"
+	kind "SharedLib"
+	language "C++"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-    }
+	pchheader "smpch.h"
+	pchsource "Soma/src/smpch.cpp"
 
-    includedirs {
-        "%{prj.name}/src",
-        "%{prj.name}/vendor/spdlog/include"
-    }
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
 
-    buildoptions{"/utf-8"}
+	includedirs {
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
 
-    filter "system:windows" 
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+	links {
+		"GLFW",
+		"opengl32.lib"
+	}
 
-        defines {
-            "SOMA_PLATFORM_WINDOWS",
-            "SOMA_BUILD_DLL"
-        }
+	buildoptions{"/utf-8"}
 
-        postbuildcommands {
-            ("{COPYFILE} %[./bin/" .. outputdir .. "/%{prj.name}/%{prj.name}.dll] %[./bin/" .. outputdir .. "/Sandbox/%{prj.name}.dll]")
-        }
+	filter "system:windows" 
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
 
-    filter "configurations:Debug"
-        defines "SOMA_DEBUG"
-        symbols "On"
+		defines {
+			"SOMA_PLATFORM_WINDOWS",
+			"SOMA_BUILD_DLL"
+		}
 
-    filter "configurations:Release"
-        defines "SOMA_RELEASE"
-        optimize "On"
+		postbuildcommands {
+			("{COPYFILE} %[./bin/" .. outputdir .. "/%{prj.name}/%{prj.name}.dll] %[./bin/" .. outputdir .. "/Sandbox/%{prj.name}.dll]")
+		}
 
-    filter "configurations:Dist"
-        defines "SOMA_DIST"
-        optimize "On"
+	filter "configurations:Debug"
+		defines "SOMA_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SOMA_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SOMA_DIST"
+		optimize "On"
 
 
 
 project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-    }
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
 
-    includedirs {
-        "Soma/vendor/spdlog/include",
-        "Soma/src"
-    }
+	includedirs {
+		"Soma/vendor/spdlog/include",
+		"Soma/src"
+	}
 
-    links {
-        "Soma"
-    }
+	links {
+		"Soma"
+	}
 
-    buildoptions{"/utf-8"}
+	buildoptions{"/utf-8"}
 
-    filter "system:windows" 
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+	filter "system:windows" 
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
 
-        defines {
-            "SOMA_PLATFORM_WINDOWS",
-        }
+		defines {
+			"SOMA_PLATFORM_WINDOWS",
+		}
 
-    filter "configurations:Debug"
-        defines "SOMA_DEBUG"
-        symbols "On"
+	filter "configurations:Debug"
+		defines "SOMA_DEBUG"
+		symbols "On"
 
-    filter "configurations:Release"
-        defines "SOMA_RELEASE"
-        optimize "On"
+	filter "configurations:Release"
+		defines "SOMA_RELEASE"
+		optimize "On"
 
-    filter "configurations:Dist"
-        defines "SOMA_DIST"
-        optimize "On"
+	filter "configurations:Dist"
+		defines "SOMA_DIST"
+		optimize "On"
